0f00bce001b2a8c62f07265f36a8c6703be29334
yerko arce <yerko.arce.galaz@gmail.com>
2023-08-06 21:54:31 -0400

    avance modulo 4

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -159,6 +159,16 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   for (var i = 2; i < num; i++){
+      if (num % i === 0){
+          return false;
+      }
+  } 
+  if (num < 2){
+      return false;
+  } else {
+      return true;
+  }
 }
 
 function esVerdadero(valor) {
